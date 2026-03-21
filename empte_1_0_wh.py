cf08d848d7f2412ba4b97606010991ccbe8d08c4
Pekooda <pekooda26082004@gmail.com>
2026-03-21 09:52:33 +0200

    TITI

--- empte_1_0_wh.py
+++ empte_1_0_wh.py
@@ -3,7 +3,7 @@
 
 #### ПЕРЕМЕННЫЕ БОТА
 ### БИБЛИОТЕКИ
-import asyncio, os
+import asyncio, os, requests, time
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -23,13 +23,14 @@ PEKO_ID = int(os.getenv("E_PEKO_ID"), 0)
 ### Начало жизни
 @dp.message(Command("start"))
 async def start(message: Message):
-    await message.answer("Я работаю. Да да.")
+    await message.answer("Я работаю и код переписан")
+    print("you get pivo")
 
 
 #### БУДИЛЬНИКИ
 async def alarms():
 ### Сообщение админу о включении
-    await bot.send_message(PEKO_ID, "зоброе утро!")
+    await bot.send_message(PEKO_ID, "ыоброе утро!")
 
 
 async def handle(request):
@@ -37,14 +38,13 @@ async def handle(request):
     await dp.feed_update(bot, data)
     return web.Response()
 
-async def on_startup(app):
-    app["alarms_task"] = asyncio.create_task(alarms())
-
 ####запись и запуск бота
 async def main():
     app = web.Application()
     app.router.add_post("/webhook", handle)
-    app.on_startup.append(on_startup)
+
+    asyncio.create_task(alarms())
+
     await bot.set_webhook("https://my-telegram-bot-on3x.onrender.com/webhook")
     runner = web.AppRunner(app)
     await runner.setup()
@@ -52,7 +52,6 @@ async def main():
     await site.start()
     await asyncio.Event().wait()
 
-    asyncio.create_task(alarms())
 
 
 
