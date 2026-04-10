f32b6c963e40d2b9ef45b389dda09ee50e0d05fe
Pekooda <pekooda26082004@gmail.com>
2026-04-10 22:37:11 +0300

    .

--- minercraftov_1_5_wh_lc.py
+++ minercraftov_1_5_wh_lc.py
@@ -1419,4 +1419,4 @@ app.on_startup.append(on_startup)
 app.on_cleanup.append(on_cleanup)
 
 if __name__ == "__main__":
-    web.run_app(app, host="0.0.0.0", port=int(os.getenv("PORT", 8080))
+    web.run_app(app, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
