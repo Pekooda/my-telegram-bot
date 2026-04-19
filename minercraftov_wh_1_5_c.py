b2c0a9fefe5eb5ccb478e0b1b28e547cc35cb8f3
Pekooda <pekooda26082004@gmail.com>
2026-04-19 23:56:22 +0300

    wow!

--- minercraftov_wh_1_5_c.py
+++ minercraftov_wh_1_5_c.py
@@ -1260,16 +1260,18 @@ async def vse(message: Message):
                 await message.reply("не зли меня, бяка >=(")
         if message.reply_to_message and message.text == "+" and message.reply_to_message.from_user.id == HURM_ID and message.chat.id == COVINOC_ID:
             if chest["hurma"]["hurmcd"]:
-                 await message.answer(f"КД на смену баллов Хурме, звиняйте, подождите немножечко :Ж")
-            chest["hurma"]["hurmball"] += 1
-            await message.answer(f"Хурме начислен балл! Текущее кол-во баллов Хурмы: {chest["hurma"]["hurmball"]}")
-            chest["hurma"]["hurmcd"] = True
+                await message.answer(f"КД на смену баллов Хурме, звиняйте, подождите немножечко :Ж")
+            else:
+                chest["hurma"]["hurmball"] += 1
+                await message.answer(f"Хурме начислен балл! Текущее кол-во баллов Хурмы: {chest["hurma"]["hurmball"]}")
+                chest["hurma"]["hurmcd"] = True
         if message.reply_to_message and message.text == "-" and message.reply_to_message.from_user.id == HURM_ID and message.chat.id == COVINOC_ID:
             if chest["hurma"]["hurmcd"]:
-                 await message.answer(f"КД на смену баллов Хурме, звиняйте, подождите немножечко :Ж")
-            chest["hurma"]["hurmball"] -= 1
-            await message.answer(f"Хурме отчислен балл! Текущее кол-во баллов Хурмы: {chest["hurma"]["hurmball"]}")
-            chest["hurma"]["hurmcd"] = True
+                await message.answer(f"КД на смену баллов Хурме, звиняйте, подождите немножечко :Ж")
+            else:
+                chest["hurma"]["hurmball"] -= 1
+                await message.answer(f"Хурме отчислен балл! Текущее кол-во баллов Хурмы: {chest["hurma"]["hurmball"]}")
+                chest["hurma"]["hurmcd"] = True
         if message.text.lower() == "баллы хурмы" and message.chat.id == COVINOC_ID:
             await message.answer(f"Текущее кол-во баллов Хурмы: {chest["hurma"]["hurmball"]}")
     if message.animation or message.sticker:
